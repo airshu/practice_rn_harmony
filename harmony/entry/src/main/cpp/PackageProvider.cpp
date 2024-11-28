7901ba0174ac8a4ea4fe442be294c28cd202aa31
longsj <longsj@xiaopeng.com>
2024-11-28 11:13:30 +0800

    entry demo

--- harmony/entry/src/main/cpp/PackageProvider.cpp
+++ harmony/entry/src/main/cpp/PackageProvider.cpp
@@ -1,7 +1,13 @@
 #include "RNOH/PackageProvider.h"
+#include "generated/rtn_calculator/RNOH/generated/BaseRtnCalculatorPackage.h"
+#include "generated/RNOHGeneratedPackage.h"
 
 using namespace rnoh;
 
 std::vector<std::shared_ptr<Package>> PackageProvider::getPackages(Package::Context ctx) {
-    return {};
+    return {
+//         std::make_shared<BaseRtnCalculatorPackage>(ctx)
+        std::make_shared<RNOHGeneratedPackage>(ctx),
+        std::make_shared<BaseRtnCalculatorPackage>(ctx)
+    };
 }
